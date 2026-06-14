4bf57f3a2dd2cd6b526fcbedc1920736b436e2f2
nishsorallikar <nishasorallikar@gmail.com>
2026-06-14 22:07:07 +0530

    "add string formatting examples for squares and cubes and tables"

---
+++ string_formatting.py
@@ -0,0 +1,20 @@
+for i  in range(1, 11):
+    print("No, {0} squared is {1} and cubed is {2}".format(i, i**2, i**3 ))
+
+print()
+
+for i  in range(1, 11):
+    print("No, {0:2} squared is {1:3} and cubed is {2:4}".format(i, i**2, i**3 ))
+    
+print()
+for i in range(1, 11):
+    print("4 X",i,"=",4*i)
+
+print()
+
+for i in range(1,11):
+    print("{0} X {1} = {2}".format(
+        4,
+        i,
+        4*i
+    ) )    
